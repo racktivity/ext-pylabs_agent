c9311abdbdfc3b226e1fdc791b95dd436da8ea12
nsami <devnull@localhost>
2009-11-10 09:30:13 +0100

    STENGLOC-1695 create branch 3.1 modify agent to call machine.registerAgent cloudapi call if no config file is available

--- agent_service/WFLAgent.py
+++ agent_service/WFLAgent.py
@@ -4,21 +4,32 @@ import base64
 
 class AgentConfig:
     def __init__(self):
-        try:
+        add = False
+        if 'main' in i.config.agent.list():
             config = i.config.agent.getConfig('main')
-        except KeyError:
-            q.logger.log("Agent failed to find the 'main' configuration: not starting.", 1)
-            raise
+        else:
+            add = True
+            con = i.config.cloudApiConnection.find('main')
+            config = con.machine.registerAgent(self._getMacaddress())['result']
 
+        self._setConfig(config)
+        if add:i.config.agent.add('main', self._getConfig())
+
+
+    def _setConfig(self, config):
         self.interval = int(config['cron_interval']) if 'cron_interval' in config else 10
         self.agentguid = config['agentguid']
         self.xmppserver = config['xmppserver']
         self.password = config['password']
         self.agentcontrollerguid = config['agentcontrollerguid']
         self.subscribed = config['subscribed'] if 'subscribed' in config else None
-        self.cronEnabled = config['enable_cron'] == 'True' if 'enable_cron' in config else True
+        self.cronEnabled = config['enable_cron'] == 'True' if 'enable_cron' in config else False
 
-    def updateConfig(self):
+    def _getMacaddress(self):
+        macaddresses = [q.system.net.getMacAddress(nic) for nic in q.system.net.getNics() if not q.system.net.getMacAddress(nic) == '00:00:00:00:00:00']
+        return macaddresses[0] or "00:00:00:00:00:00"
+
+    def _getConfig(self):
         config = dict()
         config['cron_interval'] = self.interval
         config['agentguid'] = self.agentguid
@@ -27,7 +38,10 @@ class AgentConfig:
         config['agentcontrollerguid'] = self.agentcontrollerguid
         config['subscribed'] = self.subscribed
         config['enable_cron'] = self.cronEnabled
-        i.config.agent.configure('main', config)
+        return config
+
+    def updateConfig(self):
+        i.config.agent.configure('main', self._getConfig())
 
 
 config = AgentConfig()
