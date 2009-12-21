00c99ed8f12cb24dc871f16320ef59bb2f6401f7
jnabil <john.nabil@thebetechnology.com>
2009-12-21 15:13:37 +0200

    shellcmd tasklet

--- tasklets/agent/cmds/shellcmd/tasklets/shellcmd.py
+++ tasklets/agent/cmds/shellcmd/tasklets/shellcmd.py
@@ -6,8 +6,17 @@ def match(q, i, params, tags):
     return True
 
 def main(q, i, params, tags):
-    q.logger.log("shellcmd params:%s tags:%s"%(params,tags))
-    params["returnmessage"] = 'Successfully executed command shellcmd' 
-    params["returncode"] = 0
+    q.logger.log("shellcmd params:%s tags:%s"%(params,tags), tags='tasknr:%s'%params['tasknr'])
+
+    cmd = ';'.join('\n'.join(params['params']).split('\n'))
+    output = q.system.process.executeAsync(cmd,argsInCommand=True)
+    while output.poll() == None:
+        line = output.stdout.readline()
+        if line:
+            q.logger.log(line, tags='tasknr:%s'%params['tasknr'])
+
+    params["returnmessage"] = 'Successfully executed command shellcmd :)' if output.returncode == 0 else '\n'.join(output.stderr.readlines())
+    params["returncode"] = output.returncode
+
 
 
