69cf4dde7ff78f0d19a4162c080919372a830763
abdelrahman <abdelrahman@localhost.localdomain>
2009-12-23 16:06:30 +0200

    STENGLOC-2176 Reviwer: None implement qpackages commands

--- tasklets/agent/cmds/qpackages/tasklets/setsource.py
+++ tasklets/agent/cmds/qpackages/tasklets/setsource.py
@@ -6,13 +6,20 @@ def match(q, i, params, tags):
     return params['subcmd'] == 'setsource'
 
 def main(q, i, params, tags):
-    q.logger.log("qpackages setsource params:%s tags:%s"%(params,tags))
+    q.logger.log("qpackages setsource params:%s tags:%s"%(params,tags), tags='tasknr:%s'%params['tasknr'])
     
     args = params['params']
-    q.logger.log("source: %s"%args[0])  
     
-    params["returnmessage"] = 'Successfully executed command qpackages setsource' 
-    params["returncode"] = 0
+    script ="""
+from pymonkey.inifile import IniFile
+inifile = IniFile(q.system.fs.joinPaths(q.dirs.cfgDir, 'qpackageserverlist.cfg'))
+fileAsDict = inifile.getFileAsDict()
+destItem = 'main' if 'main' in fileAsDict.keys() else fileAsDict.keys()[0]
+inifile.setParam(destItem, 'ipaddr', '%s')
+inifile.write()
+q.logger.log('QPackage server source changed for item %%s to %%s'%%(destItem, '%s'))
+"""
+    params['executeAsyncQshellCommand'](script%(args[0], args[0]), params) 
 
 
 
--- tasklets/agent/cmds/qpackages/tasklets/update.py
+++ tasklets/agent/cmds/qpackages/tasklets/update.py
@@ -6,7 +6,10 @@ def match(q, i, params, tags):
     return params['subcmd'] == 'update'
 
 def main(q, i, params, tags):
-    q.logger.log("qpackages update params:%s tags:%s"%(params,tags))
+    q.logger.log("qpackages update params:%s tags:%s"%(params,tags), tags='tasknr:%s'%params['tasknr'])
+    
+    script = '\n'.join(['q.qpackages.updateQPackageList()', 'q.qpackages.updateAllQPackages()'])
+    params['executeAsyncQshellCommand'](script, params)
     params["returnmessage"] = 'Successfully executed command qpackages update' 
     params["returncode"] = 0
 
