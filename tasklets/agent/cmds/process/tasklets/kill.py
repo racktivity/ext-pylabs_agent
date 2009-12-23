755915170598eef9897e14ee302da3f8794b1f58
abdelrahman <abdelrahman@localhost.localdomain>
2009-12-23 11:35:07 +0200

    STENGLOC-2177 Reviewer: None implement system and process cmds

--- tasklets/agent/cmds/process/tasklets/kill.py
+++ tasklets/agent/cmds/process/tasklets/kill.py
@@ -1,6 +1,7 @@
 __tags__ = 'process'
 __priority__ = 2
 
+import signal
 
 def match(q, i, params, tags):
     return params['subcmd'] == 'kill'
@@ -8,9 +9,11 @@ def match(q, i, params, tags):
 def main(q, i, params, tags):
     q.logger.log("process kill params:%s tags:%s"%(params,tags))
     
-    q.logger.log("tasknr: %s"%params['tasknr'])    
+    q.logger.log("tasknr: %s"%params['tasknr'])
+    args = params['params']
+    q.system.process.kill(int(args[0]), signal.SIGTERM)    
     
-    params["returnmessage"] = 'Successfully executed command process kill' 
+    params["returnmessage"] = 'Successfully executed command process kill for process %s'%args[0] 
     params["returncode"] = 0
 
 
--- tasklets/agent/cmds/system/tasklets/reboot.py
+++ tasklets/agent/cmds/system/tasklets/reboot.py
@@ -6,9 +6,13 @@ def match(q, i, params, tags):
     return params['subcmd'] == 'reboot'
 
 def main(q, i, params, tags):
-    q.logger.log("system reboot params:%s tags:%s"%(params,tags))
-    params["returnmessage"] = 'Successfully executed command system reboot' 
-    params["returncode"] = 0
+    q.logger.log("system reboot params:%s tags:%s"%(params,tags), tags = 'tasknr:%s'%params['tasknr'])
+    
+    cmd = 'shutdown /r' if q.platform.isWindows() else 'reboot'
+    output = q.system.process.executeAsync(cmd, argsInCommand = True)
+    output.wait()
+    params["returnmessage"] = 'Successfully executed command system reboot' if output.returncode == 0 else ''.join(output.stderr.readlines())
+    params["returncode"] = output.returncode
 
 
 
