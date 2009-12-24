406d39a71f1d1d8ee54f03b393b940d566cbff6a
abdelrahman <abdelrahman@localhost.localdomain>
2009-12-24 11:28:46 +0200

    STENGLOC-2177 Reviewer: None user pexpect agian since the passwd command has no non-interactive method of setting password in ubuntu

--- tasklets/agent/cmds/system/tasklets/setpasswd.py
+++ tasklets/agent/cmds/system/tasklets/setpasswd.py
@@ -12,11 +12,32 @@ def main(q, i, params, tags):
     username, password = args
     
     q.logger.log('username:%s, password:%s'%tuple(args))
-    cmd = 'net user %s %s'%(username, password) if q.platform.isWindows() else 'echo %s | passwd --stdin %s'%(password, username)
-    output = q.system.process.executeAsync(cmd, argsInCommand = True)
-    output.wait()
-    resultMessage = ('Password for user %s changed Successfully'%username) if output.returncode == 0 else ''.join(output.stderr.readlines())
-    returnCode = output.returncode        
+    cmd = 'net user %s %s'%(username, password)
+    returnCode = 1
+    if q.platform.isWindows():
+        output = q.system.process.executeAsync(cmd, argsInCommand = True)
+        output.wait()
+        resultMessage = ('Password for user %s changed Successfully'%username) if output.returncode == 0 else ''.join(output.stderr.readlines())
+        returnCode = output.returncode
+    else:
+        try:
+            import pexpect
+        except:
+            q.logger.log('Cannot import pexpect module, make sure that the pexpect package is installed on your system', tags='tasknr:%s'%params['tasknr'])
+            resultMessage = 'Cannot import pexpect module, make sure that the module is installed'
+        child = pexpect.spawn('passwd %s'%username)
+        if child.expect(['[Oo]ld [Pp]assword', '.current.*password', '.*[Nn]ew.*[Pp]assword']) == 0:
+            resultMessage = 'Failed to change password for user %s'%username
+        else:
+            child.sendline(password)
+            if child.expect(['[Nn]ew [Pp]assword', '[Rr]etype', '[Rr]e-enter']) == 0:
+                resultMessage = 'Failed to change password for user %s'%username
+            else:
+                child.sendline(password)
+                resultMessage = 'Password for user %s changed Successfully'%username
+                returnCode = 0
+                
+        
     
     params["returnmessage"] = resultMessage 
     params["returncode"] = returnCode
