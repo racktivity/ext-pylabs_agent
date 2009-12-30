1458f5a22ef9eb524f35168552ff7517b143c036
jnabil <john.nabil@thebetechnology.com>
2009-12-30 10:45:45 +0200

    adding agent extensions

--- agent/SchedulerClient.py
+++ agent/SchedulerClient.py
@@ -4,11 +4,8 @@ import xmlrpclib
 from pymonkey import q,i
 
 class SchedulerClient(object):
-    def __init__(self):
-        self._ip = q.config.getConfig('applicationserver')['main']['xmlrpc_ip']
-        portAsString = q.config.getConfig('applicationserver')['main']['xmlrpc_port']
-        self._port = int(portAsString) if portAsString else 0
-        self._proxy = xmlrpclib.ServerProxy('http://%(ip)s:%(port)s/'%{'ip':self._ip, 'port':self._port}, allow_none=True)
+    def __init__(self, proxy):
+        self._proxy = proxy
         
     def start(self, groupName=None):        
         self._proxy.agent_service.startScheduler(groupName)        
