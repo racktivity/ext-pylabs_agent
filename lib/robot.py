adf765e6794b2c595637bcd6a0c13a068461da35
ahussein <abdelrahman.hussein@thebetechnology.com>
2010-02-16 19:40:55 +0200

    STENGLOC-2321 Reviwer: None add try except for tasknumber generation to satrt over in case of memory not enough

--- lib/robot.py
+++ lib/robot.py
@@ -43,7 +43,7 @@ class Robot(object):
         '''
         self.COMMANDS = dict() 
         self.runningTasks = dict()
-        self._nextTaskNumber = None
+        self._nextTaskNumber = 1
         
         q.system.fswalker.walk(q.system.fs.joinPaths(q.dirs.appDir, "agent", "cmds"), callback=self._processCommandDir, arg=self.COMMANDS, includeFolders=True, recursive=False)
     
@@ -99,10 +99,12 @@ class Robot(object):
         Incrementally generate task number
         """
         
-        if not self._nextTaskNumber:
-            self._nextTaskNumber = 1
         taskNumber = self._nextTaskNumber
-        self._nextTaskNumber += 1
+        try:
+            self._nextTaskNumber += 1
+        #incase the memory of the machine is not enough for the number of take, start over
+        except:
+            self._nextTaskNumber = 1   
         return taskNumber 
         
         
