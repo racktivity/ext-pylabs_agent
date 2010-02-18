bd97736b77cc1f0a74bcefc288a9bb06a17ae14f
pgrypdon <devnull@localhost>
2010-02-18 11:08:49 +0100

    Created directory structure + empty tasklets for commands

---
+++ commands/accesscontrol/perm/list/list.py
@@ -0,0 +1,9 @@
+__author__ = 'incubaid'
+__tags__ = 'perm', 'list'
+__priority__= 1
+
+def main(q, i, params, tags):
+    params['result'] = ''
+
+def match(q, i, params, tags):
+    return True
---
+++ commands/domaincontroller/agent/setpasswd/setpasswd.py
@@ -0,0 +1,9 @@
+__author__ = 'incubaid'
+__tags__ = 'agent', 'setpasswd'
+__priority__= 1
+
+def main(q, i, params, tags):
+    params['result'] = ''
+
+def match(q, i, params, tags):
+    return True
