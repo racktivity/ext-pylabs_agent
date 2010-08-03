63432e834c7982ef616e84bcba313dd7ec1c31f0
nsami <devnull@localhost>
2010-08-03 14:52:13 +0200

    SSOBF-2300

--- commands/domaincontroller/agent/delete/delete.py
+++ commands/domaincontroller/agent/delete/delete.py
@@ -3,7 +3,14 @@ __tags__ = 'agent', 'delete'
 __priority__= 1
 
 def main(q, i, params, tags):
-    params['result'] = ''
+    agentname, domain = params['params']
+    try:
+        q.manage.servers.ejabberd.userDelete(agentname, domain)
+        params['returncode'] = 0
+        params['returnvalue'] = 'agent %s deleted successfully'%agentname
+    except:
+        params['returncode'] = -1
+        params['returnvalue'] = 'failed to delete agent %s'%agentname
 
 def match(q, i, params, tags):
     return True
